9ebda1d911c5ea6831eeaf85ce5049045c1d1747
xiaosuper163 <yabing@sfu.ca>
2019-01-06 21:31:46 -0800

    add some styles

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,21 @@
 $(function() {
+
+    // scroll to the top of the page
     $("#backToTop").on("click", function() {
         $("html, body").animate({scrollTop: 0}, 300);
     });
+
+    // toggle the content
+    $(".ac-item-hd").on("click", function(e) {
+        if ($(this).parent().children(".ac-item-bd").css("display") == "none") {
+            $(this).parent().children(".ac-item-bd").fadeIn("fast");
+        }
+        else {
+            $(this).parent().children(".ac-item-bd").slideUp("fast");
+        }
+    });
+
+    // toggle the first element on default
+    $(".myAc > :first-child").children(".ac-item-hd").trigger("click");
+
 });
